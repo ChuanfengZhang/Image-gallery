8dbf2a7d51506c91d1cd73ad3a745acf717dcbd5
Chuanfeng Zhang <wfhtzcf@gmail.com>
2016-09-21 20:57:08 +0800

    Update showPic.js
    Expanded

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,71 +1,72 @@
-function addLoadEvent(func){
+function addLoadEvent(func) {
   var oldonload = window.onload;
-  if(typeof window.onload != "function"){
+  if (typeof window.onload != "function") {
     window.onload = func;
-  }else{
-    window.onload = function (){
+  } else {
+    window.onload = function() {
       oldonload();
       func();
     }
   }
 }
-function insertAfter(newElement,targetElement){
+
+function insertAfter(newElement, targetElement) {
   var parent = targetElement.parentNode;
-  if(parent.lastChild == targetElement){
+  if (parent.lastChild == targetElement) {
     parent.appendChild(newElement);
-  }else{
-    parent.insertBefore(newElement,targetElement.nextSibling);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibling);
   }
 }
 
-function prepareGallery(){
-  if(!document.getElementsByTagName) return false;
-  if(!document.getElementById) return false;
-  if(!document.getElementById("imagegallery")) return false;
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
   var gallery = document.getElementById("imagegallery");
   var links = gallery.getElementsByTagName("a");
-  for(var i = 0; i < links.length ; i++ ){
-    links[i].onclick = function(){
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function() {
       return showPic(this);
     }
     links[i].onkeypress = links[i].onclick;
   }
 }
 
-function preparePlaceholder(){
+function preparePlaceholder() {
   if (!document.createElement) return false;
   if (!document.createTextNode) return false;
   if (!document.getElementById) return false;
   if (!document.getElementById("imagegallery")) return false;
   var placeholder = document.createElement("img");
-  placeholder.setAttribute("id","placeholder");
-  placeholder.setAttribute("src","images/yin.png");
-  placeholder.setAttribute("alt","my image gallery");
+  placeholder.setAttribute("id", "placeholder");
+  placeholder.setAttribute("src", "images/yin.png");
+  placeholder.setAttribute("alt", "my image gallery");
   var description = document.createElement("p");
-  description.setAttribute("id","description");
+  description.setAttribute("id", "description");
   var desctxt = document.createTextNode("Choose an image");
   description.appendChild(desctxt);
   var gallery = document.getElementById("imagegallery");
-  insertAfter(placeholder,gallery);
-  insertAfter(description,placeholder);
+  insertAfter(placeholder, gallery);
+  insertAfter(description, placeholder);
 }
 
 function showPic(whichPic) {
-        if(!document.getElementById("placeholder")) return false;
-        var placeholder = document.getElementById('placeholder');
-        var source = whichPic.getAttribute("href");
-        placeholder.setAttribute("src",source);
-        if(!document.getElementById("description")) return false;
-        if(whichPic.getAttribute("title")){
-          var text = whichPic.getAttribute("title");
-        }else{
-          var text = " ";
-        }
-        var description = document.getElementById("description");
-        if (description.firstChild.nodeType == 3){
-          description.firstChild.nodeValue = text;
-        }
-        return false;
-    }
- addLoadEvent(prepareGallery);
- addLoadEvent(preparePlaceholder);
+  if (!document.getElementById("placeholder")) return false;
+  var placeholder = document.getElementById('placeholder');
+  var source = whichPic.getAttribute("href");
+  placeholder.setAttribute("src", source);
+  if (!document.getElementById("description")) return false;
+  if (whichPic.getAttribute("title")) {
+    var text = whichPic.getAttribute("title");
+  } else {
+    var text = " ";
+  }
+  var description = document.getElementById("description");
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+  return false;
+}
+addLoadEvent(prepareGallery);
+addLoadEvent(preparePlaceholder);
