d42d9b8a59991bf058313963df73f1563e36f725
Chuanfeng Zhang <wfhtzcf@gmail.com>
2016-09-22 09:23:56 +0800

    Update showPic.js

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -19,20 +19,6 @@ function insertAfter(newElement, targetElement) {
   }
 }
 
-function prepareGallery() {
-  if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;
-  if (!document.getElementById("imagegallery")) return false;
-  var gallery = document.getElementById("imagegallery");
-  var links = gallery.getElementsByTagName("a");
-  for (var i = 0; i < links.length; i++) {
-    links[i].onclick = function() {
-      return showPic(this);
-    }
-    links[i].onkeypress = links[i].onclick;
-  }
-}
-
 function preparePlaceholder() {
   if (!document.createElement) return false;
   if (!document.createTextNode) return false;
@@ -50,6 +36,21 @@ function preparePlaceholder() {
   insertAfter(placeholder, gallery);
   insertAfter(description, placeholder);
 }
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this);
+    }
+    links[i].onkeypress = links[i].onclick;
+  }
+}
+
+
 
 function showPic(whichPic) {
   if (!document.getElementById("placeholder")) return false;
@@ -68,5 +69,6 @@ function showPic(whichPic) {
   }
   return false;
 }
-addLoadEvent(prepareGallery);
 addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+
